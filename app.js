4bae897728805496074c7c4b857fb41734bf664a
ammad-baig <ammadbaig50@gmail.com>
2023-03-31 02:44:53 +0500

    Assignment # 1 completed

---
+++ app.js
@@ -0,0 +1,74 @@
+//alert
+
+alert("Welcome to Assignment # 1")
+
+//Concatenating text strings
+
+var g = "Ammad ";
+var h = "Baig";
+
+var i = g + h
+console.log(i);
+
+var x = "10";
+var y = 20;
+
+var z = x + y
+console.log(z);
+// Math expressions: Familiar operators
+
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = (a + b) * (c + d);
+
+console.log(e);
+
+//Prompt
+
+var FullName = prompt("Enter Full Name")
+var Email = prompt("Enter E-mail")
+var City = prompt("Enter City", "Karachi")
+var Age = prompt("Enter Age")
+
+
+console.log(FullName + " " + Email + " From " + City + " " + Age)
+
+// increment & Decrement
+
+var ab = 100
+
+var bc = ab++
+
+console.log(bc)
+
+var ab = 100
+
+var bc = ++ab
+
+console.log(bc)
+
+var cd = 100
+
+var de = cd--
+
+console.log(de)
+
+var cd = 100
+
+var de = --cd
+
+console.log(de)
+
+// IF & ELSE
+
+var Above18 = true;
+
+if (Above18 == true) {
+    console.log("You can Enter in my Webpage")
+}
+else{
+    console.log("You are not invited in my Webpage")
+
+}
